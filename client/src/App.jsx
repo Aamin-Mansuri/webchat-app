6fb82283202c63049c752bc42909bd23c7c4200e
Aamin-Mansuri <aaminmansuri988@gmail.com>
2026-08-06 13:03:52 +0530

    links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { SocketProvider } from './context/SocketContext';
 
 
 // Pages
-import Login from './pages/Login\\';
+import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import OTPVerify from './pages/OTPVerify';
